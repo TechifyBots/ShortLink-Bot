ba524f531be74e0ab96890805326df57639a3c96
Techify Bots <TechifyBots@gmail.com>
2026-03-12 20:31:53 +0530

    Update and rename db.py to database.py

--- TechifyBots/database.py
+++ TechifyBots/database.py
@@ -12,7 +12,6 @@ db = client[DB_NAME]
 class Techifybots:
     def __init__(self):
         self.users = db["users"]
-        self.banned_users = db["banned_users"]
         self.cache: dict[int, dict[str, Any]] = {}
 
     async def add_user(self, user_id: int, name: str) -> dict[str, Any] | None:
@@ -76,31 +75,6 @@ class Techifybots:
             return result.deleted_count > 0
         except Exception as e:
             print("Error in delete_user:", e)
-            return False
-
-    async def ban_user(self, user_id: int, reason: str = None) -> bool:
-        try:
-            ban = {"user_id": user_id, "reason": reason}
-            await self.banned_users.insert_one(ban)
-            return True
-        except Exception as e:
-            print("Error in banUser:", e)
-            return False
-
-    async def unban_user(self, user_id: int) -> bool:
-        try:
-            result = await self.banned_users.delete_one({"user_id": user_id})
-            return result.deleted_count > 0
-        except Exception as e:
-            print("Error in unbanUser:", e)
-            return False
-
-    async def is_user_banned(self, user_id: int) -> bool:
-        try:
-            user = await self.banned_users.find_one({"user_id": user_id})
-            return user is not None
-        except Exception as e:
-            print("Error in isUserBanned:", e)
             return False
 
     async def set_shortner(self, user_id: int, shortner: str, api: str):
